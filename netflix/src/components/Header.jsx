9eb630f351f3347c4b74a8ddb6d5e6d819f2420f
Styles <facottry.dev@gmail.com>
2024-07-16 04:05:13 +0530

    netflix fixed

--- netflix/src/components/Header.jsx
+++ netflix/src/components/Header.jsx
@@ -1,37 +1,7 @@
 import React, { useEffect } from "react";
 import './Header.css';
 
-const Header = ({ black }) => {
-  useEffect(() => {
-    fetchConfig('FREE'); // Fetch config for FREE subscription by default
-  }, []);
-
-  const fetchConfig = async (subscriptionType) => {
-    try {
-      const response = await fetch("https://facottry-server.onrender.com/scale/get-mapping", {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          filter: { COUNTRY: 'IN', SUBSCRIPTION: subscriptionType },
-          projectID: 'vishal_72d8f604-cb87-4358-8dc8-1d53a96670c9'
-        })
-      });
-      const data = await response.json();
-      const appConfig = data.mappings.appConfig;
-      // console.log('Fetched appConfig:', appConfig);
-
-      // Update localStorage with appConfig
-      localStorage.setItem('appConfig', JSON.stringify(appConfig));
-    } catch (error) {
-      console.error('Error fetching config:', error);
-    }
-  };
-
-  // Fetch appConfig from localStorage
-  const appConfig = JSON.parse(localStorage.getItem('appConfig'));
-
+const Header = ({ black, appConfig }) => {
   return (
     <header className={black ? 'black' : ''}>
       <div className="header--logo">
