5db0c523f4e3c4bdbd3a3619a97ff26911a3c18d
Styles <facottry.dev@gmail.com>
2024-07-16 05:55:25 +0530

    added useFetch to ecom. future progress pending

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,20 +1,21 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
-import Layout from './components/layout'
-import Home from './pages/home'
-import About from './pages/about'
-import Shop from './pages/shop'
-import Contact from './pages/contact'
-import Login from './pages/login'
-import Signup from './pages/signup'
-import Forgotpasword from './pages/forgotpasword'
-import Cart from './pages/cart'
-import Checkout from './pages/checkout'
-import Blog from './pages/blog'
-import './App.css'
-import ShopContext from './components/shopcontext'
-import Details from './pages/details'
+import React from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Shop from "./pages/shop";
+import Contact from "./pages/contact";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Forgotpasword from "./pages/forgotpasword";
+import Cart from "./pages/cart";
+import Checkout from "./pages/checkout";
+import Blog from "./pages/blog";
+import "./App.css";
+import ShopContext from "./components/shopcontext";
+import Details from "./pages/details";
+import useFetchConfig from "./useFetch"
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -26,33 +27,38 @@ function ScrollToTop() {
   return null;
 }
 
-
 function App() {
+  const { appConfig, playerConfig, stale } = useFetchConfig();
 
   return (
     <>
-    <ShopContext>
-    <BrowserRouter>
-    <ScrollToTop />
-    <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
-        <Route path='blog' element={<Blog />} />
-        <Route path='about' element={<About />} />
-        <Route path='contact' element={<Contact />} />
-        <Route path='login' element={<Login />} />
-        <Route path='signup' element={<Signup />} />
-        <Route path='forgotpasword' element={<Forgotpasword />} />
-        <Route path='cart' element={<Cart />} />
-        <Route path='checkout' element={<Checkout />} />
-        <Route path='details' element={<Details />} />
-      </Route>
-    </Routes>
-    </BrowserRouter>
-    </ShopContext>
+      {stale && (
+        <div className="stale">
+          <p>No Mapping / Project Found (Rendering Default Site)</p>
+        </div>
+      )}
+      <ShopContext>
+        <BrowserRouter>
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="shop" element={<Shop appConfig={appConfig} playerConfig={playerConfig} />} />
+              <Route path="blog" element={<Blog appConfig={appConfig} playerConfig={playerConfig} />} />
+              <Route path="about" element={<About appConfig={appConfig} playerConfig={playerConfig} />} />
+              <Route path="contact" element={<Contact appConfig={appConfig} playerConfig={playerConfig} />} />
+              <Route path="login" element={<Login appConfig={appConfig} playerConfig={playerConfig} />} />
+              <Route path="signup" element={<Signup appConfig={appConfig} playerConfig={playerConfig} />} />
+              <Route path="forgotpasword" element={<Forgotpasword appConfig={appConfig} playerConfig={playerConfig} />} />
+              <Route path="cart" element={<Cart appConfig={appConfig} playerConfig={playerConfig} />} />
+              <Route path="checkout" element={<Checkout appConfig={appConfig} playerConfig={playerConfig} />} />
+              <Route path="details" element={<Details appConfig={appConfig} playerConfig={playerConfig} />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ShopContext>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ e-commerce/src/useFetch.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import fallbackData from "./fallback_data.json";
+import axios from "axios";
+
+const useFetchConfig = () => {
+  const [appConfig, setAppConfig] = useState(null);
+  const [playerConfig, setPlayerConfig] = useState(null);
+  const [stale, setStale] = useState(true);
+
+  useEffect(() => {
+    const fetchConfig = async () => {
+      try {
+        const params = new URLSearchParams(window.location.search);
+        const urlParams = Object.fromEntries(params.entries());
+        const projectID = urlParams.projectID;
+
+        if (!projectID) {
+          setAppConfig(fallbackData.mappings.appConfig);
+          setPlayerConfig(fallbackData.mappings.playerConfig);
+          setStale(true);
+          return;
+        }
+
+        const response = await axios.post(
+          "https://facottry-server.onrender.com/scale/get-mapping",
+          {
+            filter: {
+              COUNTRY: "IN",
+              SUBSCRIPTION: "FREE",
+            },
+            projectID,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        const data = response.data;
+
+        if (data.code === "FOUND") {
+          setAppConfig(data.mappings.appConfig);
+          setPlayerConfig(data.mappings.playerConfig);
+          setStale(false);
+        } else {
+          setAppConfig(fallbackData.mappings.appConfig);
+          setPlayerConfig(fallbackData.mappings.playerConfig);
+          setStale(true);
+        }
+
+      } catch (error) {
+        console.error("Error fetching live config:", error.response);
+      }
+    };
+
+    fetchConfig();
+  }, []);
+
+  return { appConfig, playerConfig, stale };
+};
+
+export default useFetchConfig;
