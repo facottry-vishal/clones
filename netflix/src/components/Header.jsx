dac87b1aa5d95c550ac1010d50d29685e21f7a1d
Vishal Arya <Facottry.vishal@gmail.com>
2024-07-23 02:00:12 +0530

    netflix config

--- netflix/src/components/Header.jsx
+++ netflix/src/components/Header.jsx
@@ -1,18 +1,26 @@
 import React, { useEffect } from "react";
 import './Header.css';
+import useStore from "../store";
 
-const Header = ({ black, appConfig }) => {
+const Header = ({ black}) => {
+  const { appConfig } = useStore();
+  if (!appConfig?.headerLogoSection) {
+    return(
+     <div>loading headerLogoSection Config</div>
+    );
+  }
   return (
     <header className={black ? 'black' : ''}>
+            {appConfig.headerLogoSection.headerMainLogo && (
+
       <div className="header--logo">
-        {appConfig && appConfig.biglogo && (
           <a href="/">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/200px-Netflix_2015_logo.svg.png" alt="Netflix Logo" />
           </a>
-        )}
-      </div>
+        
+      </div>)}
       <div className="header--user">
-        {appConfig && appConfig.smalllogo && (
+        {appConfig. headerLogoSection.headerUserLogo && (
           <a href="/">
             <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="User Avatar" />
           </a>
